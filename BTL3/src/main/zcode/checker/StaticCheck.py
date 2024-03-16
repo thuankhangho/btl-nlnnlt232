5bbb7799a5d764c9d1403c7302d389b4c25f68c4
thuankhangho <hothuankhang@gmail.com>
2024-03-16 19:30:55 +0700

    Updated Assignment 3

--- BTL3/src/main/zcode/checker/StaticCheck.py
+++ BTL3/src/main/zcode/checker/StaticCheck.py
@@ -11,3 +11,75 @@ class StaticChecker(BaseVisitor, Utils):
         
     def check(self):
         print(self.ast)
+        
+    def visitProgram(self, ast, param):
+        pass
+
+    def visitVarDecl(self, ast, param):
+        pass
+
+    def visitFuncDecl(self, ast, param):
+        pass
+
+    def visitNumberType(self, ast, param):
+        pass
+
+    def visitBoolType(self, ast, param):
+        pass
+
+    def visitStringType(self, ast, param):
+        pass
+
+    def visitArrayType(self, ast, param):
+        pass
+
+    def visitBinaryOp(self, ast, param):
+        pass
+
+    def visitUnaryOp(self, ast, param):
+        pass
+
+    def visitCallExpr(self, ast, param):
+        pass
+
+    def visitId(self, ast, param):
+        pass
+
+    def visitArrayCell(self, ast, param):
+        pass
+
+    def visitBlock(self, ast, param):
+        pass
+
+    def visitIf(self, ast, param):
+        pass
+
+    def visitFor(self, ast, param):
+        pass
+
+    def visitContinue(self, ast, param):
+        pass
+
+    def visitBreak(self, ast, param):
+        pass
+
+    def visitReturn(self, ast, param):
+        pass
+
+    def visitAssign(self, ast, param):
+        pass
+
+    def visitCallStmt(self, ast, param):
+        pass
+
+    def visitNumberLiteral(self, ast, param):
+        pass
+
+    def visitBooleanLiteral(self, ast, param):
+        pass
+
+    def visitStringLiteral(self, ast, param):
+        pass
+
+    def visitArrayLiteral(self, ast, param):
+        pass
