f072ffa76c4f08b80d968be2ec29e4d5f51a6367
thuankhangho <hothuankhang@gmail.com>
2024-03-23 01:40:16 +0700

    Started Assignment 3

--- BTL3/src/main/zcode/checker/StaticCheck.py
+++ BTL3/src/main/zcode/checker/StaticCheck.py
@@ -3,32 +3,54 @@ from Visitor import *
 from Utils import Utils
 from StaticError import *
 from functools import reduce
+from typing import Dict, List
 
+class MemberEnv:
+    pass
+
+class FuncEnv(MemberEnv):
+    pass
+
+class VarEnv(MemberEnv):
+    pass
+
+class TypeVarDynamicEnv(MemberEnv):
+    pass
+
+class LiteralEnv(MemberEnv):
+    pass
+
+class GlobalManager:
+    funcList = Dict[str, MemberEnv]
+    pass
 
 class StaticChecker(BaseVisitor, Utils):
     def __init__ (self, ast):
         self.ast = ast
         
     def check(self):
-        print(self.ast)
+        # print(self.ast)
+        param = {}
+        self.visit(self.ast, param)
         
     def visitProgram(self, ast, param):
-        pass
+        # print(ast.decl)
+        reduce(lambda _, decl: self.visit(decl, param), ast.decl, [])
 
     def visitVarDecl(self, ast, param):
-        pass
+        print(ast)
 
     def visitFuncDecl(self, ast, param):
         pass
 
     def visitNumberType(self, ast, param):
-        pass
+        return ast
 
     def visitBoolType(self, ast, param):
-        pass
+        return ast
 
     def visitStringType(self, ast, param):
-        pass
+        return ast
 
     def visitArrayType(self, ast, param):
         pass
@@ -73,13 +95,13 @@ class StaticChecker(BaseVisitor, Utils):
         pass
 
     def visitNumberLiteral(self, ast, param):
-        pass
+        return NumberLiteral()
 
     def visitBooleanLiteral(self, ast, param):
-        pass
+        return BooleanLiteral()
 
     def visitStringLiteral(self, ast, param):
-        pass
+        return StringLiteral()
 
     def visitArrayLiteral(self, ast, param):
         pass
